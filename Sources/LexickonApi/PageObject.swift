2a1428fc5aea0de17c207d7ed32ce4ec4a9e759c
Sergey Borovikov <sergey.borovikov@list.ru>
2020-09-20 12:51:45 +0500

    Added init for Page object

--- Sources/LexickonApi/PageObject.swift
+++ Sources/LexickonApi/PageObject.swift
@@ -6,7 +6,25 @@ public struct PageObject<T: Decodable>: Decodable {
         let page: Int
         let per: Int
         let total: Int
+        
+        public init(
+            page: Int,
+            per: Int,
+            total: Int
+        ) {
+            self.page = page
+            self.per = per
+            self.total = total
+        }
     }
     let metadata: Metadata
     let items: [T]
+    
+    public init(
+        metadata: Metadata,
+        items: [T]
+    ) {
+        self.metadata = metadata
+        self.items = items
+    }
 }
