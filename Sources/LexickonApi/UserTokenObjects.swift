81d204e3ddbb16ee145cf41f5b7c5cebd75e9522
Sergey Borovikov <sergey.borovikov@list.ru>
2020-09-29 18:26:20 +0500

    try add init(from decoder: Decoder)

--- Sources/LexickonApi/UserTokenObjects.swift
+++ Sources/LexickonApi/UserTokenObjects.swift
@@ -15,7 +15,13 @@ public struct UserTokenGetObject: Codable {
     }
     
     public enum CodingKeys: String, CodingKey {
-            case value
-            case userId
-        }
+        case value
+        case userId
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        value = try container.decode(String.self, forKey: .value)
+        userId = try container.decode(String.self, forKey: .userId)
+    }
 }
