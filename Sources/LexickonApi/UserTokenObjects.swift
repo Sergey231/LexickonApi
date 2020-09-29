160eb642c19b3d208ec121a0b280604b3fcc701a
Sergey Borovikov <sergey.borovikov@list.ru>
2020-09-29 18:54:29 +0500

    fixed user token object

--- Sources/LexickonApi/UserTokenObjects.swift
+++ Sources/LexickonApi/UserTokenObjects.swift
@@ -4,24 +4,24 @@ import Foundation
 public struct UserTokenGetObject: Codable {
     
     public var value: String
-    public var userId: String
+    public var id: String
     
     public init(
         value: String,
-        userId: String
+        id: String
     ) {
         self.value = value
-        self.userId = userId
+        self.id = id
     }
     
-    public enum CodingKeys: String, CodingKey {
-        case value
-        case userId
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        value = try container.decode(String.self, forKey: .value)
-        userId = try container.decode(String.self, forKey: .userId)
-    }
+//    public enum CodingKeys: String, CodingKey {
+//        case value
+//        case userId
+//    }
+//
+//    public init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        value = try container.decode(String.self, forKey: .value)
+//        userId = try container.decode(String.self, forKey: .userId)
+//    }
 }
