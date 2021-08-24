7798e9bc15779e3f7d1b7c94b6c52e7a481e8117
Sergey Borovikov <sergey.borovikov@list.ru>
2021-08-24 20:57:17 +0500

    Fixed tests

--- Tests/LexickonApiTests/LexickonApiTests.swift
+++ Tests/LexickonApiTests/LexickonApiTests.swift
@@ -6,7 +6,6 @@ final class LexickonApiTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LexickonApi().text, "Hello, World!")
     }
 
     static var allTests = [
